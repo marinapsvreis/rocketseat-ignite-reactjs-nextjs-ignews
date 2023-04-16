b3dad609082a8ee6b24f942901715125d2977318
Marina Portugal <marinapsvreis@gmail.com>
2023-04-16 12:14:30 -0300

    :passport_control: Pagina de preview do post e comportamento de redirect para quem tem inscrição ativa

--- src/pages/posts/[slug].tsx
+++ src/pages/posts/[slug].tsx
@@ -44,7 +44,7 @@ export const getServerSideProps: GetServerSideProps = async ({
   const session = await getSession({ req });
   const slug = params?.slug;
 
-  if(!session.activeSubscription){
+  if(!session?.activeSubscription){
     return {
       redirect: {
         destination: '/',
