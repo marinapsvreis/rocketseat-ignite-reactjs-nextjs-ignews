3f0819610344dcd08b56d96a3fc5bd6e57afef02
Marina Portugal <marinapsvreis@gmail.com>
2023-04-16 10:29:32 -0300

    :truck: Ajustando o comportamento do header/rotas para as duas páginas

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,14 +1,20 @@
 import { SignInButton } from '../SignInButton';
 import styles from './styles.module.scss';
+import { ActiveLink } from '../ActiveLink';
 
 export function Header(){
+
     return(
         <header className={styles.headerContainer}>
             <div className={styles.headerContent}>
                 <img src="/images/logo.svg" alt="ig.news" />
                 <nav>
-                    <a className={styles.active} href="">Home</a>
-                    <a href="">Posts</a>
+                    <ActiveLink activeClassName={styles.active} href="/" legacyBehavior>
+                        <a>Home</a>
+                    </ActiveLink>
+                    <ActiveLink activeClassName={styles.active} href="/posts" legacyBehavior>
+                        <a>Posts</a>
+                    </ActiveLink>
                 </nav>
                 <SignInButton />
             </div>
